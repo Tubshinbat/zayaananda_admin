ebf071f48c7f0ae5fc098d9b942246e887a002ae
Tubshinbat~ <tuvshinbat596@gmail.com>
2023-06-12 04:07:08 +0800

    a

--- src/base.js
+++ src/base.js
@@ -1,8 +1,8 @@
 const base = {
   siteUrl: "http://localhost:3000/",
   baseUrl: "http://localhost:3000/",
-  cdnUrl: "http://localhost:8060/uploads/",
-  apiUrl: "http://localhost:8060/api/v1/",
+  cdnUrl: "http://localhost:9999/uploads/",
+  apiUrl: "http://localhost:9999/api/v1/",
 };
 
 export default base;
